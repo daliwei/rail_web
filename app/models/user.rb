ebad34276990bf7bc3c2d788ea333f78bc591cb9
Dali Wei <dwei@MacBook-Pro.local>
2016-04-10 14:44:13 -0700

    Add new validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 250 }
 end
